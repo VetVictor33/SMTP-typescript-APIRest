17908ab04ce9a8bf7d6e89281827137519dda08d
Victor Feliciano <victorjfeliciano@gmail.com>
2023-06-30 15:37:13 -0300

    using middleware after health check

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,8 @@ import 'dotenv/config'
 import express from 'express';
 import routes from './routes';
 import cors from 'cors';
-import ValidadePassword from './middleware/ValidatePassword';
 
 export const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(new ValidadePassword().check)
-app.use(routes)
+app.use(routes);
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import Router, { Request, Response } from 'express';
 import MessageController from './controllers/MessageController';
+import ValidadePassword from './middleware/ValidatePassword';
 
 export const routes = Router();
 
-routes.get('/', (req: Request, res: Response) => res.send("App is alive"))
+routes.get('/', (req: Request, res: Response) => res.send("App is alive"));
+
+routes.use(new ValidadePassword().check);
 routes.post('/send-message', new MessageController().send);
 
 export default routes
